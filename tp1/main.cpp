846e50b7989c36273b4d6ee0675d7e4bb02285c8
pierr <pierre.besson7@gmail.com>
2011-03-16 21:33:35 +0100

    Initial Commit

---
+++ tp1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  tp1
+//
+//  Created by pierr on 16/03/11.
+//  Copyright 2011 Télécom ParisTech. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
